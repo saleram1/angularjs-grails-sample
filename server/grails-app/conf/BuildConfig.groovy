5cb4216c18bbbfb8e1ab7b79b9df0cfdcbe16d94
Michael J Salera <mikesalera@mac.com>
2014-06-21 15:16:26 -0400

    Added newer jQuery and removed logger plug-in

--- server/grails-app/conf/BuildConfig.groovy
+++ server/grails-app/conf/BuildConfig.groovy
@@ -59,7 +59,6 @@ grails.project.dependency.resolution = {
         // plugins for the compile step
         compile ":scaffolding:2.0.1"
         compile ":cache:1.1.1"
-		compile ":httplogger:1.1"
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.6" // or ":hibernate4:4.1.11.4"
