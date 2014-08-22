2995cfe91b30d026a156c4c229c9fbba10d48013
Michael J Salera <mikesalera@mac.com>
2014-08-22 19:25:51 -0400

    Added some logging at app startup

--- server/grails-app/conf/BootStrap.groovy
+++ server/grails-app/conf/BootStrap.groovy
@@ -11,8 +11,9 @@ class BootStrap {
 		new Book(title:"Java Persistence with Hibernate", author:"Gavin King", price:99.00).save()
 		new Book(title:"Spring Live", author:"Matt Raible", price:29.00).save()	
 		
+		log.info "Total Books loaded: ${Book.count()}"
 		Book.list().each { Book aBook -> 
-			println aBook
+		    log.info "Book -> ${aBook}"
 		}
     }
     def destroy = {
--- server/grails-app/conf/Config.groovy
+++ server/grails-app/conf/Config.groovy
@@ -127,7 +127,7 @@ log4j = {
 	//appenders {
 	//    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
 	//}
-
+	info   'grails.app', 'grails.jobs'
 	error  'org.codehaus.groovy.grails.web.servlet',        // controllers
 			'org.codehaus.groovy.grails.web.pages',          // GSP
 			'org.codehaus.groovy.grails.web.sitemesh',       // layouts
